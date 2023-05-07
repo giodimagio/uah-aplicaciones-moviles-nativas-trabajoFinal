bcc2d40091c52eab1f6921403f06434236e809b4
giodimagio <giodimagio@gmail.com>
2023-05-07 21:31:39 +0200

    [ViewModel] ViewModel del patrón de arquitectura MVVM

---
+++ app/src/main/java/com/example/trabajofinal/ui/viewmodel/PeliculaViewModel.java
@@ -0,0 +1,29 @@
+package com.example.trabajofinal.ui.viewmodel;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.example.trabajofinal.domain.model.Pelicula;
+import com.example.trabajofinal.data.repository.PeliculaRepository;
+
+import java.util.List;
+
+public class PeliculaViewModel extends AndroidViewModel {
+
+    private final LiveData<List<Pelicula>> listaPeliculas;
+    private final PeliculaRepository peliculaRepository;
+
+    public PeliculaViewModel(@NonNull Application application) {
+        super(application);
+        peliculaRepository = new PeliculaRepository(application);
+        listaPeliculas = peliculaRepository.obtenerListadoDePeliculas();
+    }
+
+    public LiveData<List< Pelicula>> obtenerTodasLasPeliculas() {
+        return listaPeliculas;
+    }
+
+}
