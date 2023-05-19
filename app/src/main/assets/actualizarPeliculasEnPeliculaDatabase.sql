503b3808d2494a40f4daa68b84ae0e267aed017d
giodimagio <giodimagio@gmail.com>
2023-05-20 01:34:00 +0200

    [SQL] Nuevo script para actualizar algunos datos de elementos de la tabla peliculasTabla en la base de datos peliculasdb

---
+++ app/src/main/assets/actualizarPeliculasEnPeliculaDatabase.sql
@@ -0,0 +1,7 @@
+UPDATE peliculasTabla
+SET titulo = 'Nuevo título 1', anio = 2022, duracion = '120', sinopsis = 'Nueva sinopsis', actorPrincipal = 'Pedro López'
+WHERE idPelicula = 1;
+
+UPDATE peliculasTabla
+SET titulo = 'Nuevo título 2', anio = 2023, duracion = '130', sinopsis = 'Nueva sinopsis', actorPrincipal = 'Juan Pérez'
+WHERE idPelicula = 2;
