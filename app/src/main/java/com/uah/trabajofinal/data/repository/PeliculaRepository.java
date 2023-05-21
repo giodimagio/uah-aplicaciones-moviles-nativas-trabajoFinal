a28b9edda0338794e9cee1c9e8a8963d6cdca8ac
giodimagio <giodimagio@gmail.com>
2023-05-21 22:18:48 +0200

    [PeliculaRepository] Nuevo método para borrar una pelicula y logs en cada método

--- app/src/main/java/com/uah/trabajofinal/data/repository/PeliculaRepository.java
+++ app/src/main/java/com/uah/trabajofinal/data/repository/PeliculaRepository.java
@@ -7,12 +7,15 @@ import androidx.lifecycle.LiveData;
 import com.uah.trabajofinal.data.local.dao.IPeliculaDao;
 import com.uah.trabajofinal.data.local.db.PeliculaDatabase;
 import com.uah.trabajofinal.domain.model.Pelicula;
+import com.uah.trabajofinal.utils.logger.Logger;
 
 import java.util.List;
 
 public class PeliculaRepository {
     private final LiveData<List<Pelicula>> listaPeliculas;
     private final IPeliculaDao peliculaDao;
+
+    private static final Logger logger = new Logger(PeliculaRepository.class);
 
     public PeliculaRepository(Context context) {
         PeliculaDatabase peliculaDatabaseInstance = PeliculaDatabase.obtenerInstancia(context);
@@ -21,12 +24,25 @@ public class PeliculaRepository {
     }
 
     public LiveData<List<Pelicula>> obtenerListaDePeliculas() {
+        logger.i("Obtenida lista de películas");
         return listaPeliculas;
     }
 
-    public void guardarPelicula(Pelicula Pelicula) {
+    public void guardarPelicula(Pelicula pelicula) {
         PeliculaDatabase.obtenerEjecutorDeEscrituraEnDatabase().execute(
-                () -> peliculaDao.insertarPeliculaEnDatabase(Pelicula)
+                () -> {
+                    peliculaDao.insertarPeliculaEnDatabase(pelicula);
+                    logger.i("Película " + pelicula.getTitulo() + " guardada en BBDD");
+                }
+        );
+    }
+
+    public void borrarPelicula(Pelicula pelicula) {
+        PeliculaDatabase.obtenerEjecutorDeEscrituraEnDatabase().execute(
+                () -> {
+                    peliculaDao.borrarPeliculaDeDatabase(pelicula);
+                    logger.i("Película '" + pelicula.getTitulo() + "' borrada de la BBDD");
+                }
         );
     }
 }
