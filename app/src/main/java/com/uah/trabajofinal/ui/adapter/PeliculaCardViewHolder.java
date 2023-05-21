414b305e09bec95a1811af30b68e5d445b3c2c6a
giodimagio <giodimagio@gmail.com>
2023-05-21 23:21:08 +0200

    [PeliculaCardViewHolder] Asignación del listener a cada botón y acción al hacer click. Añadidos logs

--- app/src/main/java/com/uah/trabajofinal/ui/adapter/PeliculaCardViewHolder.java
+++ app/src/main/java/com/uah/trabajofinal/ui/adapter/PeliculaCardViewHolder.java
@@ -5,17 +5,21 @@ import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.button.MaterialButton;
 import com.uah.trabajofinal.R;
 import com.uah.trabajofinal.domain.model.Pelicula;
 import com.google.android.material.imageview.ShapeableImageView;
+import com.uah.trabajofinal.utils.logger.Logger;
 
 public class PeliculaCardViewHolder extends RecyclerView.ViewHolder {
 
     private final ShapeableImageView peliculaCardCoverImage;
     private final TextView peliculaCardTitleText, peliculaCardAnioText, peliculaCardDuracionText,
             peliculaCardActorPrincipalText, peliculaCardSinopsisText;
+    private final MaterialButton materialButton1, materialButton2;
+    private static final Logger logger = new Logger(PeliculaCardViewHolder.class);
 
-    public PeliculaCardViewHolder(View itemView) {
+    public PeliculaCardViewHolder(View itemView, PeliculaCardListAdapter.OnMaterialButtonClickListener clickListener) {
         super(itemView);
 
         peliculaCardCoverImage = itemView.findViewById(R.id.peliculaCardCoverImage);
@@ -24,6 +28,26 @@ public class PeliculaCardViewHolder extends RecyclerView.ViewHolder {
         peliculaCardDuracionText = itemView.findViewById(R.id.peliculaCardDuracionText);
         peliculaCardActorPrincipalText = itemView.findViewById(R.id.peliculaCardActorPrincipalText);
         peliculaCardSinopsisText = itemView.findViewById(R.id.peliculaCardSinopsisText);
+        materialButton1 = itemView.findViewById(R.id.peliculaCardMaterialButton1);
+        materialButton2 = itemView.findViewById(R.id.peliculaCardMaterialButton2);
+
+        materialButton1.setOnClickListener(v -> {
+            int posicion = getAdapterPosition();
+            if (posicion != RecyclerView.NO_POSITION) {
+                clickListener.onMaterialButtonClick(posicion, 1);
+                logger.i("Botón '" + materialButton1.getText() + "' presionado sobre la película " +
+                        "en posición " + posicion);
+            }
+        });
+
+        materialButton2.setOnClickListener(v -> {
+            int posicion = getAdapterPosition();
+            if (posicion != RecyclerView.NO_POSITION) {
+                clickListener.onMaterialButtonClick(posicion, 2);
+                logger.i("Botón '" + materialButton2.getText() + "' presionado sobre la película " +
+                        "en posición " + posicion);
+            }
+        });
     }
 
     public void bind(Pelicula pelicula) {
