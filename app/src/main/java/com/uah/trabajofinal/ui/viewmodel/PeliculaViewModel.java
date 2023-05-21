41f1cdc3e12c20191ae43218c674608dc72f2ac9
giodimagio <giodimagio@gmail.com>
2023-05-21 23:22:33 +0200

    [PeliculaViewModel] Nuevo métodos guardar y borrar contra la clase Repository

--- app/src/main/java/com/uah/trabajofinal/ui/viewmodel/PeliculaViewModel.java
+++ app/src/main/java/com/uah/trabajofinal/ui/viewmodel/PeliculaViewModel.java
@@ -6,18 +6,19 @@ import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 
-import com.uah.trabajofinal.domain.model.Pelicula;
 import com.uah.trabajofinal.data.repository.PeliculaRepository;
+import com.uah.trabajofinal.domain.model.Pelicula;
 
 import java.util.List;
 
 public class PeliculaViewModel extends AndroidViewModel {
 
     private final LiveData<List<Pelicula>> listaPeliculasLiveData;
+    private final PeliculaRepository peliculaRepository;
 
     public PeliculaViewModel(@NonNull Application application) {
         super(application);
-        PeliculaRepository peliculaRepository = new PeliculaRepository(application);
+        peliculaRepository = new PeliculaRepository(application);
         listaPeliculasLiveData = peliculaRepository.obtenerListaDePeliculas();
     }
 
@@ -25,4 +26,11 @@ public class PeliculaViewModel extends AndroidViewModel {
         return listaPeliculasLiveData;
     }
 
+    public void guardarPelicula(Pelicula pelicula) {
+        peliculaRepository.guardarPelicula(pelicula);
+    }
+
+    public void borrarPelicula(Pelicula pelicula) {
+        peliculaRepository.borrarPelicula(pelicula);
+    }
 }
