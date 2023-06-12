2104641899d0afc4f3d1dbcc5808006d03d5be83
giodimagio <giodimagio@gmail.com>
2023-06-13 00:24:03 +0200

    [PeliculaCardListAdapter] Comentarios explicativos

--- app/src/main/java/com/uah/trabajofinal/ui/adapter/PeliculaCardListAdapter.java
+++ app/src/main/java/com/uah/trabajofinal/ui/adapter/PeliculaCardListAdapter.java
@@ -13,8 +13,19 @@ import com.uah.trabajofinal.domain.model.Pelicula;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * @author Jorge Romero
+ *
+ * @clase PeliculaCardListAdapter
+ * @descripcion Adaptador para el RecyclerView de la lista de peliculas
+ * @see RecyclerView.Adapter
+ * @see PeliculaCardViewHolder
+ */
 public class PeliculaCardListAdapter extends RecyclerView.Adapter<PeliculaCardViewHolder> {
 
+    /**
+     * Atributos, constantes y constructor del adaptador
+     */
     private List<Pelicula> listaPeliculas;
     private final OnMaterialButtonClickListener materialButtonClickListener;
 
@@ -23,11 +34,20 @@ public class PeliculaCardListAdapter extends RecyclerView.Adapter<PeliculaCardVi
         this.materialButtonClickListener = materialButtonClickListener;
     }
 
+    /**
+     * @descripcion Metodo para actualizar la lista de peliculas
+     * @param peliculas Lista de peliculas
+     */
     public void setPeliculas(List<Pelicula> peliculas) {
         this.listaPeliculas = peliculas;
         notifyDataSetChanged();
     }
 
+    /**
+     * @descripcion Metodo para obtener una pelicula de la lista según su posición en ella
+     * @param posicion Posición de la pelicula en la lista
+     * @return listaPeliculas Lista de peliculas
+     */
     public Pelicula getPeliculaEnPosicionActual(int posicion) {
         if (listaPeliculas != null && posicion < listaPeliculas.size()) {
             return listaPeliculas.get(posicion);
@@ -36,6 +56,10 @@ public class PeliculaCardListAdapter extends RecyclerView.Adapter<PeliculaCardVi
         }
     }
 
+    /**
+     * @descripcion ViewHolder que se encarga de inflar la vista de cada fragmento de pelicula
+     * @return PeliculaCardViewHolder Vista de cada fragmento de pelicula
+     */
     @NonNull
     @Override
     public PeliculaCardViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -43,6 +67,11 @@ public class PeliculaCardListAdapter extends RecyclerView.Adapter<PeliculaCardVi
         return new PeliculaCardViewHolder(view, materialButtonClickListener);
     }
 
+    /**
+     * @descripcion Metodo para enlazar los datos de cada pelicula con su vista
+     * @param holder Vista de cada fragmento de pelicula
+     * @param posicion Posición de la pelicula en la lista
+     */
     @Override
     public void onBindViewHolder(@NonNull PeliculaCardViewHolder holder, int posicion) {
         if (listaPeliculas != null && posicion < listaPeliculas.size()) {
@@ -51,11 +80,18 @@ public class PeliculaCardListAdapter extends RecyclerView.Adapter<PeliculaCardVi
         }
     }
 
+    /**
+     * @descripcion Metodo para obtener el número de peliculas de la lista
+     * @return itemCount Número de peliculas de la lista
+     */
     @Override
     public int getItemCount() {
         return listaPeliculas != null ? listaPeliculas.size() : 0;
     }
 
+    /**
+     * @descripcion Interfaz para manejar los eventos de los botones de cada fragmento de pelicula
+     */
     public interface OnMaterialButtonClickListener {
         void onMaterialButtonClick(int posicion, int tipoAccion);
     }
