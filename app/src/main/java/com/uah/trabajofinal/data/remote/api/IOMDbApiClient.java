874f01a1d5e4ae07f6c542ea39d030030dac6fe8
giodimagio <giodimagio@gmail.com>
2023-06-13 00:21:46 +0200

    [IOMDbApiClient] Comentarios explicativos

--- app/src/main/java/com/uah/trabajofinal/data/remote/api/IOMDbApiClient.java
+++ app/src/main/java/com/uah/trabajofinal/data/remote/api/IOMDbApiClient.java
@@ -6,8 +6,21 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
+/**
+ * @author Jorge Romero
+ *
+ * @interfaz IOMDbApiClient
+ * @descripcion Interfaz de acceso a la API de OMDb
+ * @see Call
+ */
 public interface IOMDbApiClient {
 
+    /**
+     * @descripcion Obtiene una película de la API OMDb por su título.
+     * @param movieTitle Título de la película.
+     * @param apiKey Clave de autorización de la API OMDb.
+     * @return PeliculaApiResponse Un objeto PeliculaApiResponse con la información de la película.
+     */
     @GET("/")
     Call<PeliculaApiResponse> getPeliculaByTitle(@Query("t") String movieTitle, @Query("apikey") String apiKey);
 }
