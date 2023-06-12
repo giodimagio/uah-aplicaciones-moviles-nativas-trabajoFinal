b1ae2de67859c31758a4849a4d14fad3b0640fe5
giodimagio <giodimagio@gmail.com>
2023-06-13 00:26:14 +0200

    [RetrofitController] Comentarios explicativos

--- app/src/main/java/com/uah/trabajofinal/data/remote/api/RetrofitController.java
+++ app/src/main/java/com/uah/trabajofinal/data/remote/api/RetrofitController.java
@@ -11,8 +11,19 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+/**
+ * @author Jorge Romero
+ *
+ * @clase RetrofitController
+ * @descripcion Controlador Retrofit como cliente REST para atacar a la API de OMDb.
+ * @see Callback
+ * @see PeliculaApiResponse
+ */
 public class RetrofitController implements Callback<PeliculaApiResponse> {
 
+    /**
+     * @descripcion Atributos, constantes y constructor de la clase.
+     */
     private static final String BASE_URL = "http://www.omdbapi.com/";
     private static final String API_KEY = "c6191c2b";
     private static final Logger logger = new Logger(RetrofitController.class);
@@ -29,11 +40,20 @@ public class RetrofitController implements Callback<PeliculaApiResponse> {
         apiClient = retrofit.create(IOMDbApiClient.class);
     }
 
+    /**
+     * @descripcion Obtiene una película de la API OMDb por su título.
+     * @param titulo Título de la película.
+     */
     public void busquedaPorTitulo(String titulo) {
         Call<PeliculaApiResponse> call = apiClient.getPeliculaByTitle(titulo, API_KEY);
         call.enqueue(this);
     }
 
+    /**
+     * @descripcion Maneja la respuesta de la API OMDb.
+     * @param call Objeto llamada, para manerar parámetros de la llamada.
+     * @param response Objeto respuesta, para manejar la respuesta de la llamada.
+     */
     @Override
     public void onResponse(@NonNull Call<PeliculaApiResponse> call, Response<PeliculaApiResponse> response) {
         if (response.isSuccessful()) {
@@ -54,6 +74,11 @@ public class RetrofitController implements Callback<PeliculaApiResponse> {
         }
     }
 
+    /**
+     * @descripcion Maneja una respuesta de error de la API OMDb.
+     * @param call Objeto llamada, para manerar parámetros de la llamada.
+     * @param t Objeto throwable, para manejar el error de la llamada.
+     */
     @Override
     public void onFailure(@NonNull Call<PeliculaApiResponse> call, Throwable t) {
         logger.i("Retrofit2 - onFailureResponse: " + t.getMessage());
