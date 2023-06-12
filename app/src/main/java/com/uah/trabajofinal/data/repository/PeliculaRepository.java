f36e83c4599d43feda8bd43209e04d728e8806b4
giodimagio <giodimagio@gmail.com>
2023-06-13 00:25:22 +0200

    [PeliculaRepository] Comentarios explicativos

--- app/src/main/java/com/uah/trabajofinal/data/repository/PeliculaRepository.java
+++ app/src/main/java/com/uah/trabajofinal/data/repository/PeliculaRepository.java
@@ -11,7 +11,17 @@ import com.uah.trabajofinal.utils.logger.Logger;
 
 import java.util.List;
 
+/**
+ * @author Jorge Romero
+ *
+ * @clase PeliculaRepository
+ * @descripcion Patrón repositorio para la clase Pelicula
+ */
 public class PeliculaRepository {
+
+    /**
+     * @descripcion Atributos, constantes y constructor de la clase
+     */
     private final LiveData<List<Pelicula>> listaPeliculas;
     private final IPeliculaDao peliculaDao;
 
@@ -23,11 +33,19 @@ public class PeliculaRepository {
         listaPeliculas = peliculaDao.obtenerListaDePeliculasEnDatabase();
     }
 
+    /**
+     * @descripcion Métodos de la clase
+     * @return listaPeliculas Objeto observable que contiene la lista de películas
+     */
     public LiveData<List<Pelicula>> obtenerListaDePeliculas() {
         logger.i("Obtenida lista de películas");
         return listaPeliculas;
     }
 
+    /**
+     * @descripcion Inserta una película en la base de datos local o actualiza una existente.
+     * @param pelicula El objeto película a insertar/actualizar.
+     */
     public void guardarPelicula(Pelicula pelicula) {
         PeliculaDatabase.obtenerEjecutorDeEscrituraEnDatabase().execute(
                 () -> {
@@ -37,6 +55,10 @@ public class PeliculaRepository {
         );
     }
 
+    /**
+     * @descripcion Borra una película de la base de datos local.
+     * @param pelicula El objeto película a borrar.
+     */
     public void borrarPelicula(Pelicula pelicula) {
         PeliculaDatabase.obtenerEjecutorDeEscrituraEnDatabase().execute(
                 () -> {
